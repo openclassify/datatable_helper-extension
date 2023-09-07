80604584a37bf6e2dd982918c44ac84e9d0686b1
emrullahardc <emrullahardc@gmail.com>
2023-09-07 14:26:49 +0300

    initial

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Datatable Helper',
+    'name'        => 'Datatable Helper Extension',
+    'description' => ''
+];
---
+++ src/DatatableHelperExtensionServiceProvider.php
@@ -0,0 +1,9 @@
+<?php namespace Visiosoft\DatatableHelperExtension;
+
+use Anomaly\Streams\Platform\Addon\AddonServiceProvider;
+use Illuminate\Routing\Router;
+
+class DatatableHelperExtensionServiceProvider extends AddonServiceProvider
+{
+
+}
